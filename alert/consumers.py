b7c665e560e16a8837ac8a57a2fa5ca9cc908175
kjh9478 <jiho9478!@gmail.com>
2018-03-04 01:30:27 +0900

    Add channels

--- alert/consumers.py
+++ alert/consumers.py
@@ -7,3 +7,45 @@
 #
 # def ws_disconnect(message):
 #     Group('users').discard(message.reply_channel)
+
+
+from channels.consumer import SyncConsumer
+from channels.generic.websocket import WebsocketConsumer
+
+
+class EchoConsumer(SyncConsumer):
+
+    def websocket_connect(self, event):
+        self.send({
+            "type": "websocket.accept",
+        })
+
+    def websocket_receive(self, event):
+        self.send({
+            "type": "websocket.send",
+            "text": event["text"],
+        })
+
+
+class MyConsumer(WebsocketConsumer):
+    groups = ["broadcast"]
+
+    def connect(self):
+        # Called on connection. Either call
+        self.accept()
+        # Or to reject the connection, call
+        # self.close()
+
+    def receive(self, text_data=None, bytes_data=None):
+        # Called with either text_data or bytes_data for each frame
+        # You can call:
+        self.send(text_data="Hello world!")
+        # Or, to send a binary frame:
+        # self.send(bytes_data="Hello world!")
+        # Want to force-close the connection? Call:
+        # self.close()
+        # Or add a custom WebSocket error code!
+        # self.close(code=4123)
+
+    def disconnect(self, close_code):
+        a = 1
--- security/routing.py
+++ security/routing.py
@@ -1,5 +1,26 @@
-from channels.routing import ProtocolTypeRouter
+# from channels.routing import ProtocolTypeRouter
+
+# application = ProtocolTypeRouter({
+#     # Empty for now (http->django views is added by default)
+# })
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.conf.urls import url
+
+from alert.consumers import MyConsumer, EchoConsumer
 
 application = ProtocolTypeRouter({
-    # Empty for now (http->django views is added by default)
+
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            # url("socket/alert/", MyConsumer),
+            url("socket/alert/", EchoConsumer),
+        ])
+    ),
+
+    # # Using the third-party project frequensgi, which provides an APRS protocol
+    # "aprs": APRSNewsConsumer,
+
 })
